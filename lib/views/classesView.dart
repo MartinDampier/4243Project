737d25bbebb67ffa179d8c4c5e985fba095aad9d
JingY789 <99154043+JingY789@users.noreply.github.com>
2023-11-09 18:34:18 -0600

    Update classesView.dart

--- lib/views/classesView.dart
+++ lib/views/classesView.dart
@@ -1,4 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/styles/genericStyles.dart';
+
+class ClassesPage extends StatefulWidget {
+  const ClassesPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _ClassesPageState createState() => _ClassesPageState();
+}
+
+class _ClassesPageState extends State<ClassesPage> {
+  List<String> inputTexts = [];
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: genericStyles.lsuPurple,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(7.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: TextField(
+                controller: textController,
+                decoration: InputDecoration(
+                  hintText: 'Enter class here',
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: inputTexts.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1.0),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 5.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(inputTexts[index],
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: IconButton(
+        icon: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          setState(() {
+            final text = textController.text;
+            if (text.isNotEmpty) {
+              inputTexts.add(text);
+              textController.clear();
+            }
+          });
+        },
+      ),
+    );
+  }
+}
+
+
+/*
+import 'package:flutter/material.dart';
 
 class ClassesPage extends StatelessWidget {
   const ClassesPage({Key? key, required this.title}) : super(key: key);
@@ -18,3 +100,11 @@ class ClassesPage extends StatelessWidget {
     );
   }
 }
+
+ */
+
+
+
+
+
+
