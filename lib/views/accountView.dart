0bafe38388151c3bac9d0e90e5c9a8c665fae2af
Ruiqi Wang <rwang26@lsu.edu>
2023-11-11 20:47:05 -0600

    icon pick and redirect to class schedule
    add the dependency of image picker and after set up user can go to class schedule page

--- lib/views/accountView.dart
+++ lib/views/accountView.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_application_1/views/classesView.dart';
+
 
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key, required this.title}) : super(key: key);
@@ -12,12 +16,24 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _subjectController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  File? _imageFile;
   String _name = '';
   String _subject = '';
   String _gender = 'Walmart Bag'; // Default gender
 
   // Gender picker items
   final List<String> _genders = ['Male', 'Female', 'Walmart Bag'];
+
+  //pick image
+  Future<void> _pickImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _imageFile = File(image.path); // Update the state to display the new image.
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,43 +42,83 @@ class _AccountPageState extends State<AccountPage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min, // Limit the size of the column
+        child: ListView(
+          padding: const EdgeInsets.all(20),
           children: <Widget>[
-            Container(
-              width: 200, // Set your desired width for text fields
-              child: TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: 'Enter your name',
-                  border: OutlineInputBorder(), // Add a border
+            if (_imageFile != null)
+              Center( // Center widget can be used to prevent horizontal stretching in ListView
+                child: ClipOval(
+                  child: Container(
+                    width: 200, // Set this to the desired width of your image.
+                    height: 200, // Set this to the desired height of your image.
+                    child: Image.file(
+                      _imageFile!,
+                      fit: BoxFit.cover, // This will cover the area of the view without changing the aspect ratio of the image.
+                    ),
+                  ),
+                ),
+              )
+            else
+              CircleAvatar(
+                child: Icon(Icons.person),
+                radius: 100,
+              ),
+
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200, // Adjust the button width here
+                child: ElevatedButton(
+                  onPressed: _pickImage,
+                  child: const Text('Upload Avatar'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // Adjust button size here
+                  ),
                 ),
               ),
             ),
-            SizedBox(height: 20), // Provide some vertical spacing
-            Container(
-              width: 200, // Set your desired width for text fields
-              child: TextField(
-                controller: _subjectController,
-                decoration: InputDecoration(
-                  labelText: 'Enter your subject',
-                  border: OutlineInputBorder(), // Add a border
-                ),
-              ),
-            ),
-            SizedBox(height: 20), // Provide some vertical spacing
-            // Gender selection buttons
-            for (var gender in _genders)
-              Container(
+            SizedBox(height: 20),
+            // Use Align to center the children and Container to constrain the width
+            Align(
+              alignment: Alignment.center,
+              child: Container(
                 width: 200,
-                color: _gender == gender ? Colors.yellow : Colors.transparent, // Highlight selected gender
+                child: TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter your name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200,
+                child: TextField(
+                  controller: _subjectController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter your subject',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ..._genders.map((gender) => Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200,
+                color: _gender == gender ? Colors.yellow : Colors.transparent,
                 child: ListTile(
                   title: Text(
                     gender,
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      color: _gender == gender ? Colors.black : Colors.grey, // Change text color based on selection
+                      color: _gender == gender ? Colors.black : Colors.grey,
                     ),
                   ),
                   onTap: () {
@@ -72,35 +128,72 @@ class _AccountPageState extends State<AccountPage> {
                   },
                 ),
               ),
-            SizedBox(height: 20), // Provide some vertical spacing
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _name = _nameController.text;
-                  _subject = _subjectController.text;
-                });
-              },
-              child: const Text('Confirm'),
+            )),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _name = _nameController.text;
+                      _subject = _subjectController.text;
+                    });
+                  },
+                  child: const Text('Confirm'),
+                ),
+              ),
             ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _nameController.text = '';
-                  _subjectController.text = '';
-                  _name = '';
-                  _subject = '';
-                  _gender = 'Walmart Bag'; // Reset the gender to default
-                });
-              },
-              child: const Text('Modify'),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200,
+                child: TextButton(
+                  onPressed: () {
+                    // Logic to clear the form
+                    setState(() {
+                      _nameController.clear();
+                      _subjectController.clear();
+                      _name = '';
+                      _subject = '';
+                      _gender = 'Not Specified'; // Reset the gender to default
+                    });
+                  },
+                  child: const Text('Reset'),
+                ),
+              ),
             ),
-            if (_name.isNotEmpty && _subject.isNotEmpty && _gender.isNotEmpty)
-              Text('Hello, $_name! Your subject is $_subject and your gender is $_gender.'),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go Back'),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 400,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Use Navigator.push to navigate to the ClassesPage
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      return const ClassesPage(title: 'My Classes');
+                    }));
+                  },
+                  child: const Text('Next, put the class schedule'),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200,
+                child: TextButton(
+                  onPressed: () {
+                    // Logic to go back to the previous screen
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Go Back'),
+                ),
+              ),
             ),
           ],
         ),
